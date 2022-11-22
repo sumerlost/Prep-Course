70a06dfe0f1470caabd28a2dc28403fa8442e318
sumerlost <Gaby_98@hotmail.com.ar>
2022-11-22 13:32:48 -0300

    empezando

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,36 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x < y)
+  {
+    return y;
+  }
+  else if (x>y)
+  {
+    return x;
+  }
+  else if (x===y)
+  {
+    var c = Math.random(x,y);
+    return c;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad => 18)
+  {
+    var c = "Allowed";
+    return c;
+  }
+  else
+  {
+    var c = "Not allowed";
+    return c;
+  }
+
 }
   
 function conection(status) {
